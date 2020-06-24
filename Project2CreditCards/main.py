4d71e35fe580194a258f6970b94fdfcebbc5d92f
Mario Kujundzic <mario.kujundzic.su@gmail.com>
2020-06-24 16:08:28 +0200

    Cluster summary data is now written into csv for each cluster; box plots for visualizing each field by cluster added

--- Project2CreditCards/main.py
+++ Project2CreditCards/main.py
@@ -1,8 +1,10 @@
 from pandas import read_csv
+import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from math import sqrt
-
+from sklearn.manifold import TSNE
+import seaborn
 
 def optimal_number_of_clusters(wcss):
     """https://jtemporal.com/kmeans-and-elbow-method/"""
@@ -41,6 +43,22 @@ def number_of_clusters(data, plot=False, min_size=2, max_size=20):
     return num_clusters
 
 
+def separate_clusters(init_data, cluster_number, plot=False):
+    clustered_data = []
+    tsne = TSNE(n_components=2)
+    for i in range(cluster_number):
+        data = init_data[init_data.cluster == i]
+        clustered_data.append(data)
+        if plot:
+            data_2d = pd.DataFrame(tsne.fit_transform(data))
+            data_2d['cluster'] = str(i)
+            data_2d.columns = ['PC1', 'PC2', 'Cluster']
+            plt.scatter('PC1', 'PC2', data=data_2d)
+    if plot:
+        plt.show()
+    return clustered_data
+
+
 if __name__ == '__main__':
     input_data = read_csv("./credit_card_data.csv")
     # pre-processing
@@ -49,11 +67,11 @@ if __name__ == '__main__':
 
     # normalization
 
-    input_data = (input_data - input_data.mean())/input_data.std()
+    # input_data = (input_data - input_data.mean())/input_data.std()
 
     # determining number of clusters
     # cluster_num = number_of_clusters(data=input_data, plot=True)
-    cluster_num = 9
+    cluster_num = 8
     # do K Means fitting
     km = KMeans(n_clusters=cluster_num, init="k-means++",
                 n_init=10, max_iter=300,
@@ -61,8 +79,15 @@ if __name__ == '__main__':
     clusters = km.fit_predict(input_data)
     # selects and labels data with cluster information
     input_data['cluster'] = clusters
-    clustered_data = []
-    for i in range(cluster_num):
-        data = input_data[input_data.cluster == i]
-        clustered_data.append(data)
-    print(clustered_data)
+    # separates clusters into a list of dataframes
+    clustered_data = separate_clusters(input_data, cluster_num, plot=False)
+    # summary for each cluster
+    for cluster in range(cluster_num):
+        print("Cluster number " + str(cluster))
+        clustered_data[cluster].describe().to_csv('cluster_' + str(cluster) + ".csv")
+    # visualizing box plots
+    cols = list(input_data.columns)
+    for col in cols:
+        data = input_data[[col, "cluster"]]
+        seaborn.boxplot(x="cluster", y=col, data=data, hue="cluster")
+        plt.show()
