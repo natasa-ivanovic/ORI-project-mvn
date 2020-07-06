545e10200ee84bc49ba7843cf82fb314c11a8105
Mario Kujundzic <mario.kujundzic.su@gmail.com>
2020-07-06 17:52:08 +0200

    Almost finalized clustering doc

--- Project2CreditCards/main.py
+++ Project2CreditCards/main.py
@@ -51,49 +51,51 @@ def separate_clusters(init_data, cluster_number, plot=False):
         data = init_data[init_data['CLUSTER'] == i]
         clustered_data.append(data)
     if plot:
-        # print("Plotting PSA")
-        # pca = PCA(n_components=2)
-        #
-        # pca_res = pd.DataFrame(data=pca.fit_transform(init_data),
-        #                        columns=['principal component 1', 'principal component 2'])
-        # pca_res = pd.concat([pca_res, init_data[["CLUSTER"]]], axis = 1)
-        # fig = plt.figure(figsize = (8,8))
-        # ax = fig.add_subplot(1,1,1)
-        # ax.set_xlabel('Principal Component 1', fontsize = 15)
-        # ax.set_ylabel('Principal Component 2', fontsize = 15)
-        # ax.set_title('2 component PCA', fontsize = 20)
-        # targets = [0, 1, 2, 3, 4, 5, 6]
-        # colors = ['r', 'g', 'b', 'c', 'y', 'm', 'k']
-        # for target, color in zip(targets,colors):
-        #     values = pca_res['CLUSTER'] == target
-        #     ax.scatter(pca_res.loc[values, 'principal component 1']
-        #                , pca_res.loc[values, 'principal component 2']
-        #                , c = color
-        #                , s = 50)
-        # ax.legend(targets)
-        # ax.grid()
+        print("Plotting PSA")
+        pca = PCA(n_components=2)
 
-        print("Plotting TSNE graph")
-        tsne = TSNE(n_components=2)
-
-        tsne_res = pd.DataFrame(data=tsne.fit_transform(init_data),
-                               columns=['x axis', 'y axis'])
-        tsne_res = pd.concat([tsne_res, init_data[["CLUSTER"]]], axis = 1)
+        pca_res = pd.DataFrame(data=pca.fit_transform(init_data),
+                               columns=['principal component 1', 'principal component 2'])
+        print(pca.explained_variance_)
+        print(pca.explained_variance_ratio_)
+        pca_res = pd.concat([pca_res, init_data[["CLUSTER"]]], axis = 1)
         fig = plt.figure(figsize = (8,8))
         ax = fig.add_subplot(1,1,1)
-        ax.set_xlabel('X axis', fontsize = 15)
-        ax.set_ylabel('Y axis', fontsize = 15)
-        ax.set_title('TSNE reduction', fontsize = 20)
+        ax.set_xlabel('Principal Component 1', fontsize = 15)
+        ax.set_ylabel('Principal Component 2', fontsize = 15)
+        ax.set_title('2 component PCA', fontsize = 20)
         targets = [0, 1, 2, 3, 4, 5, 6]
         colors = ['r', 'g', 'b', 'c', 'y', 'm', 'k']
         for target, color in zip(targets,colors):
-            values = tsne_res['CLUSTER'] == target
-            ax.scatter(tsne_res.loc[values, 'x axis']
-                       , tsne_res.loc[values, 'y axis']
+            values = pca_res['CLUSTER'] == target
+            ax.scatter(pca_res.loc[values, 'principal component 1']
+                       , pca_res.loc[values, 'principal component 2']
                        , c = color
                        , s = 50)
         ax.legend(targets)
         ax.grid()
+
+        # print("Plotting TSNE graph")
+        # tsne = TSNE(n_components=2)
+        #
+        # tsne_res = pd.DataFrame(data=tsne.fit_transform(init_data),
+        #                        columns=['x axis', 'y axis'])
+        # tsne_res = pd.concat([tsne_res, init_data[["CLUSTER"]]], axis = 1)
+        # fig = plt.figure(figsize = (8,8))
+        # ax = fig.add_subplot(1,1,1)
+        # ax.set_xlabel('X axis', fontsize = 15)
+        # ax.set_ylabel('Y axis', fontsize = 15)
+        # ax.set_title('TSNE reduction', fontsize = 20)
+        # targets = [0, 1, 2, 3, 4, 5, 6]
+        # colors = ['r', 'g', 'b', 'c', 'y', 'm', 'k']
+        # for target, color in zip(targets,colors):
+        #     values = tsne_res['CLUSTER'] == target
+        #     ax.scatter(tsne_res.loc[values, 'x axis']
+        #                , tsne_res.loc[values, 'y axis']
+        #                , c = color
+        #                , s = 50)
+        # ax.legend(targets)
+        # ax.grid()
     return clustered_data
 
 
@@ -135,9 +137,9 @@ def cluster_data(columns):
     # pre-processing
     input_data, original_data = preprocess(input_data, columns, all_cols)
     # determining number of clusters
-    # cluster_num = number_of_clusters(data=input_data, plot=True)
+    cluster_num = number_of_clusters(data=input_data, plot=True)
     # number we got
-    cluster_num = 7
+    # cluster_num = 7
     # do K Means fitting
     km = KMeans(n_clusters=cluster_num, init="k-means++",
                 n_init=10, max_iter=300,
